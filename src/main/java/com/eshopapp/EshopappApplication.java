1c54f95ddccd587800e4b1e702a5a84e922e7ef8
aamiramm <67479844+IngAamira@users.noreply.github.com>
2023-08-11 15:00:16 -0500

    Initial commit

---
+++ src/main/java/com/eshopapp/EshopappApplication.java
@@ -0,0 +1,13 @@
+package com.eshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshopapp/EshopappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
